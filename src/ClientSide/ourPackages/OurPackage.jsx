d46c8ba1fd5d6c3d92823abb8b9aa871eafde233
mahimnxt23 <mahimnxt23@gmail.com>
2025-01-30 16:22:38 +0600

    changing aba projects filenames to fix confusion for vercel v-0.3

--- src/ClientSide/ourPackages/OurPackage.jsx
+++ src/ClientSide/ourPackages/OurPackage.jsx
@@ -1,11 +1,11 @@
 import Footer from "../Shared/Footer/Footer";
-import OurPackagesBanner from "./OurPackagesBanner/ourPackagesBanner";
+import OurPackageBanner from "./OurPackagesBanner/ourPackagesBanner";
 import ServicePackage from "./ServicePackage/ServicePackage";
 
 const OurPackage = () => {
 	return (
 		<div>
-			<OurPackagesBanner></OurPackagesBanner>
+			<OurPackageBanner></OurPackageBanner>
 			<ServicePackage></ServicePackage>
 			<div className="sectionGap bg-[#1a2633]">
 				<Footer></Footer>
